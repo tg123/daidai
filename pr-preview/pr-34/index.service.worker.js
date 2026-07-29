cdec041aa25b28fb402ace5bc8685759f0618be0
tg123 <tg123@users.noreply.github.com>
2026-07-29 08:31:19 +0000

    Deploy preview for PR 34 🛫

--- pr-preview/pr-34/index.service.worker.js
+++ pr-preview/pr-34/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785313813|3078769';
+const CACHE_VERSION = '1785313836|2612416';
 /** @type {string} */
 const CACHE_PREFIX = 'DaiDai-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
